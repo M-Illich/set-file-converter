0b0b8a9d6704e8df73d087b896af90427487f190
M-Illich <moritz.illich@uni-ulm.de>
2020-03-26 14:09:14 +0100

    implementation of methods to represent a set as line in a text file

--- src/main/java/com/autoreason/setfileconverter/SetFileConverter.java
+++ src/main/java/com/autoreason/setfileconverter/SetFileConverter.java
@@ -1,23 +1,75 @@
 package com.autoreason.setfileconverter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Hashtable;
+import java.util.Set;
 
 public class SetFileConverter {
-	// current number that can be uniquely assigned to an object
+	// current number that can be used as unique ID for an object
 	private int currentNr = 0;
-	// hash table to store identifying numbers for objects
+	// hash table to store IDs for objects
 	private Hashtable<Object,Integer> hashtable = new Hashtable<Object, Integer>();
 	
 	
-	// TODO use hash table to assign objects to numbers, always check if already assigned, else get next one
-	
-	
-	// elements of set to numbers
-	
-	
-	// set to string of numbers separated by space
-	
-	
-	// collection of sets to file with each set as line
+	/**
+	 * Represent a {@link Set} by a {@link String} that contains a unique ID of each set element, separated by space, and write this string as new line to a named file
+	 * @param set A {@link Set}
+	 * @param file A {@link String} that defines the name of the file where the created set-string is stored
+	 */
+	public void writeSetToFile(Set<?> set, String file) {
+		try {
+			// create writer
+			FileWriter writer = new FileWriter(file, true);
+			BufferedWriter buffWriter = new BufferedWriter(writer);
+			
+			// convert set to string containing its elements' IDs
+			String setIDs = setToString(set);
+			// write string to file
+			buffWriter.write(setIDs);
+			buffWriter.newLine();
+			
+			buffWriter.close();
+			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
 
+	/**
+	 * Create a {@link String} that contains the IDs of each element, separated by
+	 * space, from the given {@link Set}
+	 * 
+	 * @param set A {@link Set}
+	 * @return A {@link String} of {@link int} values separated by space
+	 */
+	private String setToString(Set<?> set) {
+		StringBuilder str = new StringBuilder();
+		// go through elements of set
+		for (Object o : set) {
+			// add ID of element to string
+			str.append(getID(o) + " ");
+		}
+		return str.toString();
+	}
+	
+	
+	/**
+	 * Get the ID of an object
+	 * @param o An object
+	 * @return An {@code int} value that serves as unique identifier for the object {@code o} 
+	 */
+	private int getID(Object o){
+		// look for ID number in hashtable
+		Integer id = hashtable.get(o);
+		if(id == null) {
+			// use current number as ID for object
+			id = currentNr;
+			currentNr++;
+		}
+		return id;
+	}
+	
+	
 }
